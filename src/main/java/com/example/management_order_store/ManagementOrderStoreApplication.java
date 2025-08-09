e46aa428917d18308f7d726e348e6f0ddc2a2f16
parastooalavi <parastoo_alavi@yahoo.com>
2025-08-09 12:26:57 -0700

    last change

---
+++ src/main/java/com/example/management_order_store/ManagementOrderStoreApplication.java
@@ -0,0 +1,14 @@
+package com.example.management_order_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementOrderStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManagementOrderStoreApplication.class, args);
+        //TODO
+    }
+
+}
